8f036c697aafa62d667071a3bd0a5a3ed5ba9f72
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-02-20 04:30:27 +0300

    Completed Assignment.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 2) + 1
+
+const num3 = 9
+const num4 = 5
+
+const mod = num3 % num4;
+
+console.log(mod)
+
+const max = Math.max(1,2,3,4,5,20)
